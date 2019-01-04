5825698ded9adc33a43aa431eb4e261f8e2a6941
Jian Wei Tay <jian.tay@colorado.edu>
2019-01-04 16:01:09 -0700

    Started MATLAB wrapper
    Issue: LOADLIBRARY does not support
    structurs with pointers to structures. This
    means that the typedefs do not work.

---
+++ matlab/nd2sdk.c
@@ -0,0 +1,12 @@
+#include "mex.h"
+#include "nd2ReadSDK.h"
+
+void mexFunction(int nlhs, mxArray *plhs[],
+                 int nrhs, const mxArray *prhs[])
+{
+/* variable declarations here */
+    int i;
+
+
+/* code here */
+}
